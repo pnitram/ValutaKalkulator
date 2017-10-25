42ba0b0dae69712c0793c2b3c4ed55477f6d7bcd
pnitram <martin.pedersen@me.com>
2017-10-25 21:55:24 +0200

    V 1.3 finito

--- app/src/main/java/no/martinpedersen/valutakalkulator/MainActivity.java
+++ app/src/main/java/no/martinpedersen/valutakalkulator/MainActivity.java
@@ -1,7 +1,6 @@
 package no.martinpedersen.valutakalkulator;
 
 import android.content.SharedPreferences;
-import android.content.res.Configuration;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -14,7 +13,6 @@ import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.Spinner;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
@@ -41,23 +39,13 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         mValutaInput = (EditText) findViewById(R.id.eTekst);
         mErrorMsg = (TextView) findViewById(R.id.errorMsg);
         mErrorMsg.setTextColor(Color.RED);
-        //mButton1 = (Button) findViewById(R.id.button1);
-        //mButton2 = (Button) findViewById(R.id.button2);
         mButton3 = (Button) findViewById(R.id.button3);
-        //mRadioButton1 = (RadioButton) findViewById(R.id.rNE);
-        //mRadioButton2 = (RadioButton) findViewById(R.id.rEN);
-        //mValutaInput.setOnClickListener(this);
-
 
         if (savedInstanceState != null) {
             //Saved as instanceState
             mValutaInput.setText(savedInstanceState.getString(INPUT_TEXT_VALUE));
             mValutaView.setText(savedInstanceState.getString(OUTPUT_TEXT_VALUE));
 
-            //Saved as settings
-            SharedPreferences settings = getSharedPreferences("Radio", 0);
-            //mRadioButton1.setChecked(settings.getBoolean("radioA", false));
-            //mRadioButton2.setChecked(settings.getBoolean("radioB", false));
         }
 
         //Spinner
@@ -70,34 +58,34 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
     }
 
-    protected void nokToEurBase() {
+    protected void exchangeMoney() {
 
         try {
             Double input = Double.parseDouble(mValutaInput.getText().toString());
             Double output;
             switch (spinnerValue) {
                 case "NOK to EUR":
-                    output = input/8.5;
+                    output = input/9.4;
                     mValutaView.setText(String.format("%.2f", output) + " EUR");
                     break;
                 case "EUR to NOK":
-                    output = input/8.5;
+                    output = input*9.4;
                     mValutaView.setText(String.format("%.2f", output) + " NOK");
                     break;
                 case "NOK to USD":
-                    output = input/8.5;
+                    output = input/8;
                     mValutaView.setText(String.format("%.2f", output) + " USD");
                     break;
                 case "USD to NOK":
-                    output = input/8.5;
+                    output = input*8;
                     mValutaView.setText(String.format("%.2f", output) + " NOK");
                     break;
                 case "NOK to GBP":
-                    output = input/8.5;
+                    output = input/10.6;
                     mValutaView.setText(String.format("%.2f", output) + " GBP");
                     break;
                 case "GBP to NOK":
-                    output = input/8.5;
+                    output = input*10.6;
                     mValutaView.setText(String.format("%.2f", output) + " NOK");
                     break;
             }
@@ -108,31 +96,6 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
             mErrorMsg.setText(R.string.errorInput);
         }
     }
-
-    protected void eurToNokBase() {
-        try {
-            Double input = Double.parseDouble(mValutaInput.getText().toString());
-            Double output = input*8.5;
-            mValutaView.setText(String.format("%.2f", output) + " NOK");
-        } catch (NumberFormatException e) {
-            e.printStackTrace();
-            Log.d("TAG","Error eurToNok");
-            mErrorMsg.setText(R.string.errorInput);
-        }
-    }
-
-    protected void nokToEur() {
-
-        Log.d("TAG","NOKtoEUR pressed");
-        nokToEurBase();
-    }
-
-    protected void eurToNok(View view) {
-
-        Log.d("TAG","EURtoNOK pressed");
-        eurToNokBase();
-    }
-
 
 
     protected void clearOnClick(View view) {
@@ -146,24 +109,11 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
     }
 
-
     public void veksle(View view) {
 
         mErrorMsg.setText("");
-        //mValutaView.setText(spinnerValue);
-        nokToEurBase();
-
-       /* if (mRadioButton1.isChecked()) {
-            nokToEurBase();
-        }
-        else if (mRadioButton2.isChecked()) {
-            eurToNokBase();
-        }
-        else if (!mRadioButton1.isChecked() || !mRadioButton2.isChecked()){
-            mErrorMsg.setText(R.string.errorInput);
-        }*/
+        exchangeMoney();
     }
-
 
     @Override
     protected void onSaveInstanceState (Bundle outState) {
@@ -172,26 +122,18 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         outState.putString(INPUT_TEXT_VALUE, mValutaInput.getText().toString());
         outState.putString(OUTPUT_TEXT_VALUE, mValutaView.getText().toString());
 
-        //Saved as settings
-/*        SharedPreferences settings = getSharedPreferences("Radio", 0);
-        SharedPreferences.Editor editor = settings.edit();
-        editor.putBoolean("radio1", mRadioButton1.isChecked());
-        editor.putBoolean("radio2", mRadioButton2.isChecked());
-        editor.commit();*/
-
-        }
+    }
 
     @Override
     public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
 
         spinnerValue = adapterView.getItemAtPosition(i).toString();
-
-
     }
 
     @Override
     public void onNothingSelected(AdapterView<?> adapterView) {
 
+        //Nothing
     }
 }
 
